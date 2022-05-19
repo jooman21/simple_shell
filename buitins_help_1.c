1887c14f301a400cf2c1e0afbcd7fb14e45142b8
Mehamud-salih <mahmudsalih96@gmail.com>
2022-05-19 22:00:55 +0300

    simple shell project

--- buitins_help_1.c
+++ buitins_help_1.c
@@ -1,18 +1,15 @@
 #include "shell.h"
-
 void help_all(void);
 void help_alias(void);
 void help_cd(void);
 void help_exit(void);
 void help_help(void);
-
 /**
  * help_all - Displays all possible builtin shellby commands.
  */
 void help_all(void)
 {
 char *msg = "Shellby\nThese shell commands are defined internally.\n";
-
 write(STDOUT_FILENO, msg, _strlen(msg));
 msg = "Type 'help' to see this list.\nType 'help name' to find ";
 write(STDOUT_FILENO, msg, _strlen(msg));
@@ -27,14 +24,12 @@ write(STDOUT_FILENO, msg, _strlen(msg));
 msg = "unsetenv [VARIABLE]\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
 }
-
 /**
  * help_alias - Displays information on the shellby builtin command 'alias'.
  */
 void help_alias(void)
 {
 char *msg = "alias: alias [NAME[='VALUE'] ...]\n\tHandles aliases.\n";
-
 write(STDOUT_FILENO, msg, _strlen(msg));
 msg = "\n\talias: Prints a list of all aliases, one per line, in ";
 write(STDOUT_FILENO, msg, _strlen(msg));
@@ -49,14 +44,12 @@ write(STDOUT_FILENO, msg, _strlen(msg));
 msg = "is already an alias, replace its value with VALUE.\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
 }
-
 /**
  * help_cd - Displays information on the shellby builtin command 'cd'.
  */
 void help_cd(void)
 {
 char *msg = "cd: cd [DIRECTORY]\n\tChanges the current directory of the";
-
 write(STDOUT_FILENO, msg, _strlen(msg));
 msg = " process to DIRECTORY.\n\n\tIf no argument is given, the ";
 write(STDOUT_FILENO, msg, _strlen(msg));
@@ -69,14 +62,12 @@ write(STDOUT_FILENO, msg, _strlen(msg));
 msg = "after a change of directory.\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
 }
-
 /**
  * help_exit - Displays information on the shellby builtin command 'exit'.
  */
 void help_exit(void)
 {
 char *msg = "exit: exit [STATUS]\n\tExits the shell.\n\n\tThe ";
-
 write(STDOUT_FILENO, msg, _strlen(msg));
 msg = "STATUS argument is the integer used to exit the shell.";
 write(STDOUT_FILENO, msg, _strlen(msg));
@@ -85,14 +76,12 @@ write(STDOUT_FILENO, msg, _strlen(msg));
 msg = " exit 0.\n";
 write(STDOUT_FILENO, msg, _strlen(msg));
 }
-
 /**
  * help_help - Displays information on the shellby builtin command 'help'.
  */
 void help_help(void)
 {
 char *msg = "help: help\n\tSee all possible Shellby builtin commands.\n";
-
 write(STDOUT_FILENO, msg, _strlen(msg));
 msg = "\n      help [BUILTIN NAME]\n\tSee specific information on each ";
 write(STDOUT_FILENO, msg, _strlen(msg));
