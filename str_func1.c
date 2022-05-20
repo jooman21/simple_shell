411c6dba23b2952c50ed14f1f4d62d9c4864f6a5
Mehamud-salih <mahmudsalih96@gmail.com>
2022-05-20 14:09:31 +0300

    string function

--- str_func1.c
+++ str_func1.c
@@ -1,10 +1,8 @@
 #include "shell.h"
-
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
-
 /**
  * _strchr - Locates a character in a string.
  * @s: The string to be searched.
@@ -16,16 +14,13 @@ int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strchr(char *s, char c)
 {
 int index;
-
 for (index = 0; s[index]; index++)
 {
 if (s[index] == c)
 return (s + index);
 }
-
 return (NULL);
 }
-
 /**
  * _strspn - Gets the length of a prefix substring.
  * @s: The string to be searched.
@@ -38,7 +33,6 @@ int _strspn(char *s, char *accept)
 {
 int bytes = 0;
 int index;
-
 while (*s)
 {
 for (index = 0; accept[index]; index++)
@@ -52,7 +46,6 @@ break;
 }
 return (bytes);
 }
-
 /**
  * _strcmp - Compares two strings.
  * @s1: The first string to be compared.
@@ -69,13 +62,10 @@ while (*s1 && *s2 && *s1 == *s2)
 s1++;
 s2++;
 }
-
 if (*s1 != *s2)
 return (*s1 - *s2);
-
 return (0);
 }
-
 /**
  * _strncmp - Compare two strings.
  * @s1: Pointer to a string.
@@ -89,7 +79,6 @@ return (0);
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
 size_t i;
-
 for (i = 0; s1[i] && s2[i] && i < n; i++)
 {
 if (s1[i] > s2[i])
