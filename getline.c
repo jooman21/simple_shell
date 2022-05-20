3ed0472e9cafee3eb9da9a1e8311797f0f97ff41
Mehamud-salih <mahmudsalih96@gmail.com>
2022-05-20 11:41:46 +0300

    getline

--- getline.c
+++ getline.c
@@ -1,9 +1,7 @@
 #include "shell.h"
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-
 /**
  * _realloc - Reallocates a memory block using malloc and free.
  * @ptr: A pointer to the memory previously allocated.
@@ -19,7 +17,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 void *mem;
 char *ptr_copy, *filler;
 unsigned int index;
-
 if (new_size == old_size)
 return (ptr);
 if (ptr == NULL)
@@ -30,13 +27,11 @@ return (NULL);
 
 return (mem);
 }
-
 if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
 }
-
 ptr_copy = ptr;
 mem = malloc(sizeof(*ptr_copy) * new_size);
 if (mem == NULL)
@@ -44,16 +39,12 @@ if (mem == NULL)
 free(ptr);
 return (NULL);
 }
-
 filler = mem;
-
 for (index = 0; index < old_size && index < new_size; index++)
 filler[index] = *ptr_copy++;
-
 free(ptr);
 return (mem);
 }
-
 /**
  * assign_lineptr - Reassigns the lineptr variable for _getline.
  * @lineptr: A buffer to store an input string.
@@ -85,7 +76,6 @@ _strcpy(*lineptr, buffer);
 free(buffer);
 }
 }
-
 /**
  * _getline - Reads input from a stream.
  * @lineptr: A buffer to store the input.
@@ -100,17 +90,14 @@ static ssize_t input;
 ssize_t ret;
 char c = 'x', *buffer;
 int r;
-
 if (input == 0)
 fflush(stream);
 else
 return (-1);
 input = 0;
-
 buffer = malloc(sizeof(char) * 120);
 if (!buffer)
 return (-1);
-
 while (c != '\n')
 {
 r = read(STDIN_FILENO, &c, 1);
@@ -124,17 +111,13 @@ if (r == 0 && input != 0)
 input++;
 break;
 }
-
 if (input >= 120)
 buffer = _realloc(buffer, input, input + 1);
-
 buffer[input] = c;
 input++;
 }
 buffer[input] = '\0';
-
 assign_lineptr(lineptr, n, buffer, input);
-
 ret = input;
 if (r != 0)
 input = 0;
